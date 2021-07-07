2dcd531107ba4d0fb5d128850c91aaaa5ed4696e
vrhaynes <vrhaynes.tech@gmail.com>
2021-07-07 14:34:30 -0700

    Updates from in person tutorial

--- brain_package/utils.py
+++ brain_package/utils.py
@@ -19,20 +19,4 @@ def get_spaced_colors(n):
 
 
 
-def gauss(x, y, center=(5, 5), sigma=3.5):
-    '''
-        Creates a 2-D gaussian.
-        
-        INPUT
-        - x, y (float) = range of xs and ys
-        - center (tuple/(float,float)) = mean of the gaussian
-        - sigma (float) = standard deviation of gaussian
-        
-        OUTPUT
-        - (float) = value of the gaussian at (x,y)
-    
-    '''
-    dist = ((x-center[0])**2 + (y-center[1])**2) ** 0.5
-    return 100 * np.exp(-dist**2 / sigma)
-
 
